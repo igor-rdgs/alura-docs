a84c4c00b79d8bd05df8b7c10b4e60cee84c4ab2
Igor Rodrigues <igor.aguiar1@live.com>
2025-12-05 14:41:51 -0300

    atualizando arquivos

--- public/socket-front-documento.js
+++ public/socket-front-documento.js
@@ -1,9 +1,11 @@
-import { atualizaTextoEditor } from "./documento.js";
+import { alertarERedirecionar, atualizaTextoEditor } from "./documento.js";
 
 const socket = io();
 
 function selecionarDocumento(nome) {
-	socket.emit("selecionar_documento", nome);
+	socket.emit("selecionar_documento", nome, (texto) => {
+		atualizaTextoEditor(texto);
+	});
 };
 
 function emitirTextoEditor(dados) {
@@ -14,9 +16,12 @@ socket.on("texto_editor_clientes", (texto) => {
 	atualizaTextoEditor(texto);
 });
 
-socket.on("disconnect", (motivo) => {
-	console.log(`Servidor desconectado!
-	Motivo: ${motivo}`);
+function emitirExcluirDocumento(nome) {
+	socket.emit("excluir_documento", nome);
+};
+
+socket.on("excluir_documento_sucesso", (nome) => {
+	alertarERedirecionar(nome);
 })
 
-export { emitirTextoEditor, selecionarDocumento };
+export { emitirTextoEditor, selecionarDocumento, emitirExcluirDocumento };
---
+++ src/dbConnect.js
@@ -0,0 +1,21 @@
+import { MongoClient } from "mongodb";
+
+const cliente = new MongoClient(
+	"mongodb+srv://alura:123@aluracluster.sg61scb.mongodb.net/?appName=AluraCluster"
+);
+
+let documentosColecao;
+
+try {
+	await cliente.connect();
+
+	const db = cliente.db("alura-websockets");
+	documentosColecao = db.collection("documentos");
+
+	console.log("Conectado ao banco de dados com sucesso!");
+
+} catch (erro) {
+	console.log(erro);
+}
+
+export { documentosColecao };
